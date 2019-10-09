3af8ee23cb0c2fcef87dc1365e05367677c0d4a5
Reymezis <qq1s@bk.ru>
2019-10-09 17:40:59 +0600

    add game brain-calc

---
+++ src/engine.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace  BrainGames\engine;
+
+use function BrainGames\Cli\run;
+use function cli\line;
+use function cli\prompt;
+
+function welcome($greeting)
+{
+    line("Welcome to the Brain Games!");
+    return line("{$greeting}\n");
+}
+
+function sayHi()
+{
+    $userName = prompt('May I have your name?');
+    run($userName);
+    return $userName;
+}
+
+function generatorNumbers($num)
+{
+    for ($i = 0; $i < $num; $i++) {
+        $array[] = rand(1, 20);
+    }
+    return $array;
+}
+
+function symbolsGenerator()
+{
+    $symbols = [" + "," - "," * "];
+    shuffle($symbols);
+    return $symbols;
+}
+
+function engine($correct, $exercises, $userName)
+{
+    for ($j = 0; $j < 3; $j++) {
+        line("Question: {$exercises[$j]}");
+        $userAnswer = prompt("Your answer");
+        $correctAnswer = $correct[$j];
+        if ($correctAnswer === $userAnswer) {
+            line("Correct!");
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $userAnswer, $correctAnswer);
+            return line("Let's try again, %s!", $userName);
+        }
+    }
+    return line("Congratulations, %s!\n", $userName);
+}
